c9cb8c83fd23c0de33a198a5a20752ec93e8b26c
taufik545 <63764188+taufik545@users.noreply.github.com>
2021-10-07 17:50:39 +0700

    Menyelesaikan video ke 2

---
+++ resources/views/about.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+  <title>WPU BLOG |about </title>
+  <h3>Taufik hidayat </h3>
+  <p>Taufik193019@gmail.com</p>
+  <img src="img/Taufik.jpeg" alt="Taufik Hidayat" width="200">
+</head>
+<body>
+  <h1> halaman about </h1>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>WPU BLOG |  posts </title>
+</head>
+<body>
+  <h1> halaman posts </h1>
+</body>
+</html>
