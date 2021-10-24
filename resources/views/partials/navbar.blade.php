ce74b366e7f21cd8445b5bfb5879b25dc2256e63
taufik545 <63764188+taufik545@users.noreply.github.com>
2021-10-24 15:28:27 +0700

    menyelesaikan vidio blade templating engine

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+  <div class="container">
+    <a class="navbar-brand" href="/">WPU BLOG</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link {{ ($title === "Home") ? 'active' :'' }} "  href="/home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{ ($title === "About") ? 'active' :'' }} " href="/about">about</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link {{ ($title === "posts") ? 'active' :'' }} " href="/blog">Blog</a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" hre f="#">Something else here</a></li>
+          </ul>
+        </li>
+       
+        
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -14,13 +14,74 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/home', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
-     return view('about');
+    return view('about', [
+        "title" => "About",
+        "name" => "Taufik hidayat",
+        "email" => "Taufikhidayat@unpas.ac.id",
+        "image" => "Taufik.jpeg"
+
+
+
+    ]);
 });
 
+
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "judul post pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Taufik hidayat",
+            "body" => "lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis doloremque, porro optio perspiciatis mollitia delectus, fugiat ratione adipisci dolore alias dolores nisi animi iure explicabo eos tempora incidunt, obcaecati minus fuga accusamus autem vel praesentium expedita? Facilis cumque fugit quasi porro doloribus quaerat, molestiae minima repellat autem laudantium eaque libero laborum at optio quos. Voluptatem porro exercitationem, deleniti iusto molestiae possimus eius assumenda praesentium voluptates! Adipisci inventore quibusdam deserunt commodi, impedit quae ipsa veritatis, totam perspiciatis maiores distinctio, consequatur facilis?"
+        ],
+        [
+            "title" => "judul post kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "herlan nurachman",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae nam debitis, autem ab, repellat reiciendis placeat consequatur aliquam incidunt maxime, laboriosam accusantium obcaecati quasi consectetur maiores earum unde. Reprehenderit laudantium placeat reiciendis vel commodi maxime ipsum, amet consequuntur repellendus aspernatur! Officiis sint rerum itaque animi ipsa voluptas laborum magni quod repellat ullam vitae blanditiis eligendi nostrum nisi aperiam, est dolore, libero ducimus tempore iste deserunt in dolores facilis! Rerum quas, perferendis facilis consequatur fugit officia quo deleniti voluptatem asperiores ratione obcaecati nobis exercitationem laboriosam tenetur suscipit sequi adipisci ab. Praesentium nisi, at sequi quidem ad ipsum exercitationem nulla labore officiis."
+        ],
+    ];
+
+    return view('posts', [
+        "title" => "posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+//halaman sigle post
+
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "judul post pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Taufik hidayat",
+            "body" => "lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis doloremque, porro optio perspiciatis mollitia delectus, fugiat ratione adipisci dolore alias dolores nisi animi iure explicabo eos tempora incidunt, obcaecati minus fuga accusamus autem vel praesentium expedita? Facilis cumque fugit quasi porro doloribus quaerat, molestiae minima repellat autem laudantium eaque libero laborum at optio quos. Voluptatem porro exercitationem, deleniti iusto molestiae possimus eius assumenda praesentium voluptates! Adipisci inventore quibusdam deserunt commodi, impedit quae ipsa veritatis, totam perspiciatis maiores distinctio, consequatur facilis?"
+        ],
+        [
+            "title" => "judul post kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "herlan nurachman",
+            "body" => "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Recusandae nam debitis, autem ab, repellat reiciendis placeat consequatur aliquam incidunt maxime, laboriosam accusantium obcaecati quasi consectetur maiores earum unde. Reprehenderit laudantium placeat reiciendis vel commodi maxime ipsum, amet consequuntur repellendus aspernatur! Officiis sint rerum itaque animi ipsa voluptas laborum magni quod repellat ullam vitae blanditiis eligendi nostrum nisi aperiam, est dolore, libero ducimus tempore iste deserunt in dolores facilis! Rerum quas, perferendis facilis consequatur fugit officia quo deleniti voluptatem asperiores ratione obcaecati nobis exercitationem laboriosam tenetur suscipit sequi adipisci ab. Praesentium nisi, at sequi quidem ad ipsum exercitationem nulla labore officiis."
+        ],
+    ];
+
+    $neW_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $neW_post = $post;
+        }
+    }
+    return view('post', [
+        "title" => "Single post",
+        "post" => $neW_post
+    ]);
 });
